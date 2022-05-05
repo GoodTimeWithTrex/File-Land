ec582d3ef8b91daf29593101ad71dd17a2d48ed9
GoodTimeWithTrex <105020317+GoodTimeWithTrex@users.noreply.github.com>
2022-05-05 17:50:13 -0500

    Matf files

---
+++ World.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int Best()
+{
+    int poopoo = 7 * 887 / 239;
+    int Trevor = 3 * 123 / 4;
+    int Tucker = Trevor * poopoo;
+    return Tucker;
+}
+int main()
+{
+    int Tucker = Best();
+    std::cout << ("%i", Tucker);
+    return 0;
+}
